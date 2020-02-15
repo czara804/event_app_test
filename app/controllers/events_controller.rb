80a616e65e4dfa028c8f4d4711af116527e26d03
czara804 <cbzara@gmail.com>
2020-02-15 12:27:17 +1100

    added nav

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :find_event, only: [:show, :edit, :update, :destroy]
 
   def index
